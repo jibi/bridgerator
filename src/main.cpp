a4d7abd742c9f41282c307e945556480527bdc47
jibi <jibi@paranoici.org>
2013-06-26 22:17:51 +0200

    start listeners

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,28 @@
 #include<iostream>
+#include<unordered_map>
+
 #include<bridgerator/config>
+#include<bridgerator/network>
 
 config *bt_config;
+std::unordered_map<int, listener  *> listeners;
 
 int main() {
 	bt_config = new config();
 
 	for (int i = 0; i < bt_config->services_count(); i++) {
-		/* start listeners */
+		int sd;
+		listener *l;
+		
+		l = new listener(bt_config->service_at(i).local_port);
+		sd = l->start();
+
+		if (sd == -1) {
+			std::cout << "Failed to bind on port " << bt_config->service_at(i).local_port << std::endl;
+			continue;
+		}
+
+		listeners[sd] = l;
 	}
 
 	while (1) {
